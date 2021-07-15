a6f684f385e5e28be48ac56d52c5d543a2d2d569
Irfan Asif <hashtag.irfan@gmail.com>
2021-07-15 12:48:22 +0530

    added modules folder

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+
+const replaceTemplate = require('./modules/replaceTemplate')
 // const slugify = require('slugify');
 // const replaceTemplate = require('./modules/replaceTemplate');
 
@@ -34,19 +36,7 @@ const url = require("url");
 /////////////////////////////////
 // SERVER
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName); //replace template function will replace all the templates with names, image etc from product array
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
+
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,"utf-8");
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,"utf-8");
